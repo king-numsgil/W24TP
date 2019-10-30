dddc768278dd2dd89ab785f1462a3bab2b42afd6
Alexis <exerapidoXthorn@gmail.com>
2019-10-30 14:29:25 -0400

    Created Messages & Responses controllers

---
+++ W24TP/Controllers/MessagesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using W24TP.Models;
+
+namespace W24TP.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private ForumONIEntities db = new ForumONIEntities();
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+            var messages = db.Messages.Include(m => m.AspNetUser).Include(m => m.Category);
+            return View(messages.ToList());
+        }
+
+        // GET: Messages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.CatID = new SelectList(db.Categories, "CatID", "CategoryName");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MsgID,MsgTitle,MsgText,CatID,UserID,CreationDate,View,IsActive")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", message.UserID);
+            ViewBag.CatID = new SelectList(db.Categories, "CatID", "CategoryName", message.CatID);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", message.UserID);
+            ViewBag.CatID = new SelectList(db.Categories, "CatID", "CategoryName", message.CatID);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MsgID,MsgTitle,MsgText,CatID,UserID,CreationDate,View,IsActive")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", message.UserID);
+            ViewBag.CatID = new SelectList(db.Categories, "CatID", "CategoryName", message.CatID);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Message message = db.Messages.Find(id);
+            db.Messages.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ W24TP/Controllers/ReponsesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using W24TP.Models;
+
+namespace W24TP.Controllers
+{
+    public class ReponsesController : Controller
+    {
+        private ForumONIEntities db = new ForumONIEntities();
+
+        // GET: Reponses
+        public ActionResult Index()
+        {
+            var reponses = db.Reponses.Include(r => r.AspNetUser).Include(r => r.Message);
+            return View(reponses.ToList());
+        }
+
+        // GET: Reponses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reponse reponse = db.Reponses.Find(id);
+            if (reponse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reponse);
+        }
+
+        // GET: Reponses/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle");
+            return View();
+        }
+
+        // POST: Reponses/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RepID,RepText,MsgID,UserID,CreationDate,IsActive")] Reponse reponse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reponses.Add(reponse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", reponse.UserID);
+            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle", reponse.MsgID);
+            return View(reponse);
+        }
+
+        // GET: Reponses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reponse reponse = db.Reponses.Find(id);
+            if (reponse == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", reponse.UserID);
+            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle", reponse.MsgID);
+            return View(reponse);
+        }
+
+        // POST: Reponses/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RepID,RepText,MsgID,UserID,CreationDate,IsActive")] Reponse reponse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reponse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", reponse.UserID);
+            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle", reponse.MsgID);
+            return View(reponse);
+        }
+
+        // GET: Reponses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reponse reponse = db.Reponses.Find(id);
+            if (reponse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reponse);
+        }
+
+        // POST: Reponses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reponse reponse = db.Reponses.Find(id);
+            db.Reponses.Remove(reponse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
