31e095b0d3a888473d2becd7728d6ee379f38bc0
Daniel Grondin <king@numsgil.co>
2019-11-06 14:39:09 -0500

    Added a new Helper for string manipulation

---
+++ W24TP/Helpers/PrettyShortText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace W24TP.Helpers
+{
+    public static class PrettyShortText
+    {
+        public static string Print(string message, int num_words)
+        {
+            return string.Join(" ", message.Split().Take(num_words)) + "...";
+        }
+    }
+}
