d203ddd76ff47101702ce003d7448a204b69d35a
Daniel Grondin <king@numsgil.co>
2019-11-18 14:19:57 -0500

    Removed useless shitless stuffless

--- W24TP/Controllers/ReponsesController.cs
+++ W24TP/Controllers/ReponsesController.cs
@@ -15,38 +15,6 @@ namespace W24TP.Controllers
     {
         private ForumONIEntities db = new ForumONIEntities();
 
-        // GET: Reponses
-        public ActionResult Index()
-        {
-            var reponses = db.Reponses.Include(r => r.AspNetUser).Include(r => r.Message);
-            return View(reponses.ToList());
-        }
-
-        // GET: Reponses/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Reponse reponse = db.Reponses.Find(id);
-            if (reponse == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(reponse);
-        }
-
-        // GET: Reponses/Create
-        public ActionResult Create()
-        {
-            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
-            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle");
-            return View();
-        }
-
         [HttpPost]
         [Authorize]
         [ValidateInput(false)]
@@ -65,65 +33,6 @@ namespace W24TP.Controllers
             db.SaveChanges();
 
             return RedirectToAction("Details", "Messages", new { id, path });
-        }
-
-        // POST: Reponses/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
-        /*[HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "RepID,RepText,MsgID,UserID,CreationDate,IsActive")]
-            Reponse reponse)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Reponses.Add(reponse);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", reponse.UserID);
-            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle", reponse.MsgID);
-            return View(reponse);
-        }*/
-
-        // GET: Reponses/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Reponse reponse = db.Reponses.Find(id);
-            if (reponse == null)
-            {
-                return HttpNotFound();
-            }
-
-            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", reponse.UserID);
-            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle", reponse.MsgID);
-            return View(reponse);
-        }
-
-        // POST: Reponses/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "RepID,RepText,MsgID,UserID,CreationDate,IsActive")]
-            Reponse reponse)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(reponse).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", reponse.UserID);
-            ViewBag.MsgID = new SelectList(db.Messages, "MsgID", "MsgTitle", reponse.MsgID);
-            return View(reponse);
         }
 
         // GET: Reponses/Delete/5
